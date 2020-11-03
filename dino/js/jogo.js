899382dedf0202d3eeb6461480a756165b2b9f21
Eduardo Gonçalves Ribeiro <eduardo_g_ribeiro@estudante.sc.senai.br>
2020-11-03 16:42:11 -0300

    Movimentar cacto

--- dino/js/jogo.js
+++ dino/js/jogo.js
@@ -21,7 +21,7 @@ function pular(){
                     console.log('chão');
                     estaPulando = false;
                     clearInterval(intervaloQueda);
-                    
+
                 } else {
                     //Decrementar a posição do Dino 
                     dinoPosition -= 20;
@@ -38,3 +38,29 @@ function pular(){
         }
     }  ,20);
 };
+
+//Criação de Cactos
+const background = document.querySelector('.background');
+
+let cactoPosition = 1000;
+function criarCacto () {
+    const cacto = document.createElement('div');
+    let tempoRandom = Math.random() * 6000 + 100; 
+    cacto.classList.add('cacto');
+    cacto.style.left = 1000 + 'px';
+    // aparece o cacto na tela 
+    background.appendChild(cacto);
+
+    let intervaloEsquerda = setInterval(()=>{
+        if (cactoPosition <= -60) {
+            clearInterval(intervaloEsquerda);
+            //Remover da tela
+            background.removeChild(cacto);
+        } else{  
+            cactoPosition -= 10;
+            cacto.style.left = cactoPosition + 'px';
+        }
+    },20);
+    let tempoCacto = setTimeout(criarCacto, tempoRandom);
+}
+criarCacto();
